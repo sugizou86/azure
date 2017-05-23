0c7bb80f11875836a294e9b11152dc26bde4d53a
sugizou86 <k.sugi2941@hue.ac.jp>
2017-05-23 12:31:38 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+# azure
+const HTTP = require('http');
+const PORT = process.env.PORT || 3001;
+
+HTTP.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js!!!!!!\n');
+}).listen(PORT);
